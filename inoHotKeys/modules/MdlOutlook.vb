7c6bb425fe78762f169036103862dd9ac2dc8e83
INOPIAE <m.maengel@inopiae.de>
2023-05-20 11:58:49 +0200

    upd: bring new email to front

--- inoHotKeys/modules/MdlOutlook.vb
+++ inoHotKeys/modules/MdlOutlook.vb
@@ -3,16 +3,18 @@ Imports System.Runtime.InteropServices
 Imports Outlook = Microsoft.Office.Interop.Outlook
 Module MdlOutlook
     Public Sub CreateEmail()
-        Dim outApp As Outlook.Application
-        Dim outMail As Outlook.MailItem
+        Dim olApp As Outlook.Application
+        Dim olMail As Outlook.MailItem
 
         If Process.GetProcessesByName("OUTLOOK").Count() > 0 Then
             'here is the part where i get the error in the second part
-            outApp = DirectCast(ExMarshal.GetActiveObject("Outlook.Application"), Outlook.Application)
+            olApp = DirectCast(ExMarshal.GetActiveObject("Outlook.Application"), Outlook.Application)
         Else
-            outApp = New Microsoft.Office.Interop.Outlook.Application
+            olApp = New Microsoft.Office.Interop.Outlook.Application
         End If
-        outMail = outApp.CreateItem(Outlook.OlItemType.olMailItem)
-        outMail.Display()
+        olMail = olApp.CreateItem(Outlook.OlItemType.olMailItem)
+        Dim olInspector As Outlook.Inspector = olMail.GetInspector
+        olMail.Display()
+        olInspector.Activate()
     End Sub
 End Module
